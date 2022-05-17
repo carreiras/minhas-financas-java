f59457ff881edde10420e49516aa777716cd8855
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-05-17 17:04:30 -0300

    Adicionando os Mocks nos testes de UsuarioService

--- src/test/java/com/carreiras/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/carreiras/minhasfinancas/service/UsuarioServiceTest.java
@@ -1,12 +1,13 @@
 package com.carreiras.minhasfinancas.service;
 
 import com.carreiras.minhasfinancas.exception.RegraNegocioException;
-import com.carreiras.minhasfinancas.model.entity.Usuario;
 import com.carreiras.minhasfinancas.model.repository.UsuarioRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -16,20 +17,19 @@ public class UsuarioServiceTest {
     @Autowired
     UsuarioService usuarioService;
 
-    @Autowired
+    @MockBean
     UsuarioRepository usuarioRepository;
 
     @Test
     public void deveValidarEmail() {
-        usuarioRepository.deleteAll();
+        Mockito.when(usuarioRepository.existsByEmail(Mockito.anyString())).thenReturn(false);
 
         usuarioService.validarEmail("usuario@email.com");
     }
 
     @Test()
     public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
-        Usuario usuario = Usuario.builder().nome("Usuário").email("usuario@email.com").build();
-        usuarioRepository.save(usuario);
+        Mockito.when(usuarioRepository.existsByEmail(Mockito.anyString())).thenReturn(true);
 
         Throwable erroValidacaoEmail = Assertions
                 .catchThrowable(() -> usuarioService.validarEmail("usuario@email.com"));
