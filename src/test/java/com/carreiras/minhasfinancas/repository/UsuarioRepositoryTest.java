b82cebbeb0671f186f4ea6802b82835bb46f2a1d
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-05-17 13:47:06 -0300

    Adicionando banco de dados em memória para os testes de integração

--- src/test/java/com/carreiras/minhasfinancas/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/carreiras/minhasfinancas/repository/UsuarioRepositoryTest.java
@@ -7,11 +7,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioRepositoryTest {
 
     @Autowired
