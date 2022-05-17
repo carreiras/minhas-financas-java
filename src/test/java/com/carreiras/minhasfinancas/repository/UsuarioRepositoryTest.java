977b0ac5a5302125c9cc8e628bdd9d063d34cf8a
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-05-17 15:56:13 -0300

    Melhorando nossos testes de integração com @DataJpaTest

--- src/test/java/com/carreiras/minhasfinancas/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/carreiras/minhasfinancas/repository/UsuarioRepositoryTest.java
@@ -8,11 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@DataJpaTest
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
